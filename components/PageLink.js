0e9061280b3e86b7b8081134396dde778f5bc459
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-07-23 16:59:23 +0900

    [clean]propsの整理

--- components/PageLink.js
+++ components/PageLink.js
@@ -5,9 +5,8 @@ import styles from '../styles/Home.module.css'
 export function PageLink(props) {
 
   return (
-        <div className={styles.pageLinkBtn}>
-          <p>{props.link1}</p>
-          <p>{props.link2}</p>
+        <div className={styles.pageLink}>
+          {props.link}
         </div>
   )
 }
