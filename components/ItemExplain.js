08a23823d6c893e1d421e639247d87c61f325009
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-07-25 15:16:36 +0900

    [clean]cssのコンポーネント化

---
+++ components/ItemExplain.js
@@ -0,0 +1,32 @@
+import styles from '../styles/Home.module.css'
+
+
+export function ItemExplain(props) {
+
+const IMG =['img/item1.jpg','img/item2.jpg','img/item3.jpg','img/item4.jpg',
+  'img/item5.jpg','img/item6.jpg','img/item7.jpg','img/item8.jpg',
+  'img/item9.jpg','img/item10.jpg','img/item11.jpg','img/item12.jpg',
+  'img/item13.jpg','img/item14.jpg','img/item15.jpg','img/item1.jpg'
+]
+
+  return (
+      <div className={styles.itemPege}>
+        <p>プロダクトタイトル</p>
+        <div className={styles.itemExplain}>
+        <div className={styles.itemImg}>
+            <img src={IMG[props.img]}/>
+        </div>
+        <div className={styles.itemText}>
+            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            <p>¥99,999 +tax</p>
+            <p>
+            SIZE：W999 × D999 × H999<br/>
+            COLOR：テキスト<br/>
+            MATERIAL：テキストテキストテキスト<br/>
+            </p>
+        </div>
+        </div>
+      </div>
+  )
+}
--- pages/item1.js
+++ pages/item1.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import Link from 'next/link'
+import { ItemExplain } from '../components/ItemExplain'
 
 
 
@@ -20,24 +21,7 @@ export default function item1() {
       <Header />
 
       <main className={styles.main}>
-      <div className={styles.itemPege}>
-        <p>プロダクトタイトル</p>
-        <div className={styles.itemExplain}>
-        <div className={styles.itemImg}>
-            <img src='img/item1.jpg'/>
-        </div>
-        <div className={styles.itemText}>
-            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-            <p>¥99,999 +tax</p>
-            <p>
-            SIZE：W999 × D999 × H999<br/>
-            COLOR：テキスト<br/>
-            MATERIAL：テキストテキストテキスト<br/>
-            </p>
-        </div>
-        </div>
-      </div>
+        <ItemExplain  img={0}/>
 
       <div className={styles.pageLinkBtn}>
         <Link href="/products">
