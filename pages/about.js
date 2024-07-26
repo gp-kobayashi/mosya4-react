f487d86fd5436253b510f3f4420341c3da4bda00
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-07-26 17:00:21 +0900

    [new]aboutページの追加

--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
-import Link from 'next/link'
-import { ItemExplain } from '../components/ItemExplain'
 
 
 
